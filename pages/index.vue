c2b5644375e28154e79211cb53e99b9ec77c77d9
hcijr6 <hcijr6@gmail.com>
2023-11-27 18:34:36 +0100

    REMOVE PAGES + ISR IN EVERY ROUTE

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+const generatedAt = useState(() => new Date().toISOString())
+const date = new Date(generatedAt.value)
+const timeAgo = ref()
+onMounted(() => {
+    timeAgo.value = ms(Date.now() - date.valueOf(), { long: true })
+})
+</script>
+
+<template>
+    <div>
+        {{ timeAgo }}
+    </div>
+</template>
